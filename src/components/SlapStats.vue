f34d88690829dfed8b289d09d8a8ed0ba3058e7c
Angus <angus.goody@gmail.com>
2025-06-12 18:11:52 +0100

    new ui

--- src/components/SlapStats.vue
+++ src/components/SlapStats.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="text-center">
-    <p class="mt-4 text-lg">
-      You've slapped Elon
-      <span class="font-mono font-bold text-red-800">{{ storeCount }}</span>
-      times.
-    </p>
+  <div>
+    <dl class="mt-5 grid grid-cols-2 gap-4 sm:grid-cols-3">
+      <!-- Your Slaps -->
+      <div class="overflow-hidden rounded-lg bg-white px-4 py-5 sm:p-6">
+        <dt class="truncate text-sm font-medium text-gray-500">Your slaps</dt>
+        <dd class="mt-1 text-xl font-mono tracking-tight text-red-900">
+          {{ storeCount }}
+        </dd>
+      </div>
 
-    <div class="my-20">
-      <p class="text-md text-gray-700">Global Slaps</p>
-      <p><span class="text-xl font-mono">{{ globalSlaps }}</span></p>
-    </div>
+      <!-- Rank -->
+      <div class="overflow-hidden rounded-lg bg-white px-4 py-5 sm:p-6">
+        <dt class="truncate text-sm font-medium text-gray-500">Your Rank</dt>
+        <dd class="mt-1 text-xl font-mono tracking-tight text-gray-900">
+          {{ rank }} / {{ totalUsers }}
+        </dd>
+      </div>
 
-    <div class="my-10">
-      Your rank is
-      <span class="font-mono">{{ rank }}</span> out of
-      <span class="font-mono">{{ totalUsers }}</span> users.
-    </div>
+      <!-- Global Slaps (spans 2 cols on mobile) -->
+      <div class="col-span-2 sm:col-span-1 overflow-hidden rounded-lg bg-white px-4 py-5  sm:p-6">
+        <dt class="truncate text-sm font-medium text-gray-500">Global slaps</dt>
+        <dd class="mt-1 text-xl font-mono tracking-tight text-gray-900">
+          {{ globalSlaps }}
+        </dd>
+      </div>
+    </dl>
   </div>
+
+
 </template>
 
 <script setup lang="ts">
