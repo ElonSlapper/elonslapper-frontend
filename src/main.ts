6677a81474fa51b8cd869da07945265faeb6c94b
Angus <angus.goody@gmail.com>
2025-06-13 11:50:54 +0100

    added migration script

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
+import { migrateLegacySlapStore } from './util/migrate'
+migrateLegacySlapStore()
+
 import App from '@/App.vue'
 import router from '@/router'
 
