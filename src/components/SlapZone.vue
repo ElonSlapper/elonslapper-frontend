a13dfd2e6a77b67433402919ebbcd8f5af1c4f84
Angus <angus.goody@gmail.com>
2025-06-13 10:22:24 +0100

    new pinia schema & offline support

--- src/components/SlapZone.vue
+++ src/components/SlapZone.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="flex flex-col items-center gap-y-5 md:gap-y-10">
+    <div class="rounded-md bg-red-50 p-4">
+      <div class="flex">
+        <div class="shrink-0">
+          <svg class="size-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+            <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="ml-3">
+          <h3 class="text-sm font-medium text-red-800">You are offline!</h3>
+          <div class="mt-2 text-sm text-red-700">
+            <p>Global slaps and ranks will update when you are back online</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <GlobalCount :globalSlaps="formattedGlobalSlaps" />
     <SlapImage
       :image="currentImage"
@@ -30,6 +46,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const currentImage = ref(elonImage)
 const isCursorClicked = ref(false)
+const isOnline = ref(navigator.onLine)
 let timeout: ReturnType<typeof setTimeout> | null = null
 let interval: ReturnType<typeof setInterval>
 
@@ -71,7 +88,11 @@ async function sendSlaps(count: number) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ slap_count: count, user_id: store.userId }),
     })
-    if (res.ok) store.markSubmitted()
+    if (res.ok){
+      store.markSubmitted()
+      console.log(`✅ Sent ${count} slaps successfully!`)
+    }
+
   } catch (err) {
     console.error('❌ Error sending slaps:', err)
   }
@@ -137,15 +158,33 @@ async function periodicFetch() {
   }
 }
 
+function handleOnline() {
+  isOnline.value = true
+  console.log('🟢 Back online')
+  if (store.getUnsentSlaps() > 0) {
+    sendSlaps(store.getUnsentSlaps())
+  }
+}
+
+function handleOffline() {
+  isOnline.value = false
+  console.log('🔴 Offline')
+}
+
+
 onMounted(() => {
   fetchUserId()
   periodicFetch()
   interval = setInterval(periodicFetch, 6000)
   document.addEventListener('visibilitychange', handleVisibilityChange)
+  window.addEventListener('online', handleOnline)
+  window.addEventListener('offline', handleOffline)
 })
 
 onUnmounted(() => {
   clearInterval(interval)
   document.removeEventListener('visibilitychange', handleVisibilityChange)
+  window.removeEventListener('online', handleOnline)
+  window.removeEventListener('offline', handleOffline)
 })
 </script>
